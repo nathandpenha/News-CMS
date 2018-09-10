15e6c98699545a5ca715c932313f4e994e056ea7
Nathan Zion D'Penha <dpenhanathan@yahoo.in>
2018-09-11 00:19:19 +0530

    topbar

--- admin/includes/topbar.php
+++ admin/includes/topbar.php
@@ -13,7 +13,7 @@
 						<span class=" fa fa-angle-down"></span>
 					</a>
 					<ul class="dropdown-menu dropdown-usermenu pull-right">
-						<li><a href="javascript:;"> Profile</a></li>
+						<li><a href="../admin/profile.php"> Profile</a></li>
 						<li><a href="../../logout.php"><i class="fa fa-sign-out pull-right"></i>Log Out</a></li>
 					</ul>
 				</li>
--- includes/base.php
+++ includes/base.php
@@ -4,9 +4,9 @@
 		define("DB_USER","root");
 		define("DB_PASS","");
 		define("DB_NAME","demo");
-		define("DB_PREFIX", "aka_");
+		define("DB_PREFIX", "na_");
 		$db = new mysqli(DB_HOST , DB_USER , DB_PASS, DB_NAME);
-
+		
 	$siteName = $db->query("SELECT meta_value FROM ".DB_PREFIX."site_meta WHERE `meta_name` = 'WebSiteName'")->fetch_assoc()['meta_value'];
 	$siteDescription = $db->query("SELECT meta_value FROM ".DB_PREFIX."site_meta WHERE `meta_name` = 'WebSiteDescription'")->fetch_assoc()['meta_value'];
 	$userAccess = $db->query("SELECT meta_value FROM ".DB_PREFIX."site_meta WHERE `meta_name` = 'UserAccount'")->fetch_assoc()['meta_value'];
@@ -17,3 +17,4 @@
 			header('Location: ../admin/login.php');
 		}
 	}
+	
